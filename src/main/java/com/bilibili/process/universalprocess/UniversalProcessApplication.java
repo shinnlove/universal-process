07679dae605654c0282897be81aea589e55a9f5a
zhaochensheng <306086640@qq.com>
2022-02-16 14:45:00 +0800

    initialize empty springboot project

---
+++ src/main/java/com/bilibili/process/universalprocess/UniversalProcessApplication.java
@@ -0,0 +1,13 @@
+package com.bilibili.process.universalprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalProcessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bilibili/process/universalprocess/UniversalProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilibili.process.universalprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
